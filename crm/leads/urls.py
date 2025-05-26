2e4f183b80745a21b4706e846f3a6fe9c09df377
Umesh <umeshtamang129@gmail.com>
2025-05-26 07:11:38 +0545

    lead delete view

--- crm/leads/urls.py
+++ crm/leads/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('leads/',views.lead_list,name='list'),
     path('detail/<int:id>/',views.lead_detail,name='detail'),
     path('create/',views.lead_create,name='create'),
-    path('update/<int:id>/',views.lead_update,name='update')
+    path('update/<int:id>/',views.lead_update,name='update'),
+    path('delete/<int:id>/',views.lead_delete,name='delete')
 ]
