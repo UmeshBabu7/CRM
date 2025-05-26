6962a0c5440365b06b42ae1067992c91d7c24aa9
Umesh <umeshtamang129@gmail.com>
2025-05-26 17:47:30 +0545

    sending email

--- crm/leads/views.py
+++ crm/leads/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, reverse
 from .models import Lead, Agent
 from .forms import LeadModelForm
 from django.views import generic
-
+from django.core.mail import send_mail
 
 
 # Create your views here.
@@ -86,6 +86,15 @@ class LeadCreateView(generic.CreateView):
 
     def get_success_url(self):
         return reverse("leads:lead-list")
+    
+    def form_valid(self, form):
+        send_mail(
+            subject="A lead has been created",
+            message="Go to the site to see the new lead",
+            from_email="test@test.com",
+            recipient_list=["test2@test.com"]
+        )
+        return super(LeadCreateView, self).form_valid(form)
     
 
 class LeadUpdateView(generic.UpdateView):
