9a449b1fb2c5eb02c26b38adc2e4f1696df359f5
Umesh <umeshtamang129@gmail.com>
2025-05-27 16:44:55 +0545

    authentication

--- crm/leads/urls.py
+++ crm/leads/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import (
     LeadListView, LeadDetailView, LeadCreateView, LeadUpdateView, LeadDeleteView
 )
+from django.contrib.auth.views import LoginView, LogoutView
+from leads.views import LandingPageView, SignupView
 
 app_name="leads"
 
@@ -11,4 +13,8 @@ urlpatterns = [
     path('update/<int:pk>/', LeadUpdateView.as_view(), name='lead-update'),
     path('delete/<int:pk>/', LeadDeleteView.as_view(), name='lead-delete'),
     path('create/', LeadCreateView.as_view(), name='lead-create'),
+
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- crm/leads/views.py
+++ crm/leads/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, reverse
 from .models import Lead, Agent
-from .forms import LeadModelForm
+from .forms import LeadModelForm, CustomUserCreationForm
 from django.views import generic
 from django.core.mail import send_mail
+
 
 
 # Create your views here.
@@ -113,3 +114,17 @@ class LeadDeleteView(generic.DeleteView):
 
     def get_success_url(self):
         return reverse("leads:lead-list")
+    
+
+
+
+
+
+class SignupView(generic.CreateView):
+    template_name = "registration/signup.html"
+    form_class = CustomUserCreationForm
+
+    def get_success_url(self):
+        return reverse("leads:login")
+    
+
