a3ab5663738486d6ee8ea35d169f9a236118c112
Umesh <umeshtamang129@gmail.com>
2025-05-28 20:14:21 +0545

    test

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -1,14 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path,include
-from leads.views import LandingPageView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('leads/',include('leads.urls')),
-    path('', LandingPageView.as_view(), name='landing-page'),
+    path('',include('leads.urls')),
+
 ]
 
 if settings.DEBUG:
--- crm/leads/urls.py
+++ crm/leads/urls.py
@@ -8,7 +8,8 @@ from leads.views import LandingPageView, SignupView
 app_name="leads"
 
 urlpatterns = [
-    path('', LeadListView.as_view(), name='lead-list'),
+    path('', LandingPageView.as_view(), name='landing-page'),
+    path('list/', LeadListView.as_view(), name='lead-list'),
     path('detail/<int:pk>/', LeadDetailView.as_view(), name='lead-detail'),
     path('update/<int:pk>/', LeadUpdateView.as_view(), name='lead-update'),
     path('delete/<int:pk>/', LeadDeleteView.as_view(), name='lead-delete'),
