e462a2b2bbadc2068f1f0ec97aa080de260b9037
Umesh <umeshtamang129@gmail.com>
2025-06-05 15:04:23 +0545

    Lead Model Form

--- crm/leads/views.py
+++ crm/leads/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Lead, Agent
-from .forms import LeadForm
+from .forms import LeadModelForm
 
 # Create your views here.
 
@@ -24,15 +24,11 @@ def lead_detail(request,id):
 
 
 def lead_create(request):
-    form=LeadForm()
+    form=LeadModelForm()
     if request.method == 'POST':
-        form=LeadForm(request.POST)
+        form=LeadModelForm(request.POST)
         if form.is_valid():
-            first_name=form.cleaned_data['first_name']
-            last_name=form.cleaned_data['last_name']
-            age=form.cleaned_data['age']
-            agent=Agent.objects.first()
-            Lead.objects.create(first_name=first_name,last_name=last_name,age=age,agent=agent)
+            form.save()
             return redirect('leads:lead_list')
     context={
             'form':form
