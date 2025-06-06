0f4912bcbbf2ed7a3e19340ea6aa8426e8a0f9b5
Umesh <umeshtamang129@gmail.com>
2025-06-06 21:00:43 +0545

    Sending email

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -112,6 +112,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = "static_root"
 
 AUTH_USER_MODEL="leads.User"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- crm/leads/views.py
+++ crm/leads/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Lead, Agent
 from .forms import LeadModelForm
 from django.views import generic
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -79,7 +80,16 @@ class leadCreateView(generic.CreateView):
     form_class=LeadModelForm
 
     def get_success_url(self):
-        return reverse("leads:lead-list")
+        return reverse("leads:lead-list") 
+    
+    def form_valid(self, form):
+        send_mail(
+            subject="A lead has been created",
+            message="Go to the site to see the new lead",
+            from_email="test@test.com",
+            recipient_list=["test2@test.com"]
+        )
+        return super(leadCreateView,self).form_valid(form)
     
 
 class LeadDetailView(generic.DetailView):
