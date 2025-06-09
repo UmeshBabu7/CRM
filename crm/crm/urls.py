4b31f5e7ef20419074070d5c6a16b44d207b87ed
Umesh <umeshtamang129@gmail.com>
2025-06-09 10:18:50 +0545

    Agent list and create view

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('leads/',include('leads.urls'))
+    path('leads/',include('leads.urls')),
+    path('agents/',  include('agents.urls',)),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
