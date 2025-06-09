9d5edcb0e20d65533a98b526d48d5670e7d39055
Umesh <umeshtamang129@gmail.com>
2025-06-09 08:44:04 +0545

    Login required mixin

--- crm/leads/views.py
+++ crm/leads/views.py
@@ -4,6 +4,7 @@ from .models import Lead, Agent
 from .forms import LeadModelForm, CustomUserCreationForm
 from django.views import generic
 from django.core.mail import send_mail
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -69,13 +70,13 @@ class LandingPageView(generic.TemplateView):
     template_name="landing.html"
 
 
-class LeadListView(generic.ListView):
+class LeadListView(LoginRequiredMixin,generic.ListView):
     template_name="leads/lead_list.html"
     queryset=Lead.objects.all()
     context_object_name="leads"
 
 
-class leadCreateView(generic.CreateView):
+class leadCreateView(LoginRequiredMixin,generic.CreateView):
     template_name="leads/lead_create.html"
     form_class=LeadModelForm
 
@@ -92,13 +93,13 @@ class leadCreateView(generic.CreateView):
         return super(leadCreateView,self).form_valid(form)
     
 
-class LeadDetailView(generic.DetailView):
+class LeadDetailView(LoginRequiredMixin,generic.DetailView):
     template_name="leads/lead_detail.html"
     queryset=Lead.objects.all()
     context_object_name="lead"
 
 
-class LeadUpdateView(generic.UpdateView):
+class LeadUpdateView(LoginRequiredMixin,generic.UpdateView):
     template_name="leads/lead_update.html"
     queryset=Lead.objects.all()
     form_class=LeadModelForm
@@ -107,7 +108,7 @@ class LeadUpdateView(generic.UpdateView):
         return reverse("leads:lead-list")
     
 
-class LeadDeleteView(generic.DeleteView):
+class LeadDeleteView(LoginRequiredMixin,generic.DeleteView):
     template_name="leads/lead_delete.html"
     queryset=Lead.objects.all()
 
