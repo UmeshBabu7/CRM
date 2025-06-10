cf00f7c041aadc33d59b3e278da01b79a224bb05
Umesh <umeshtamang129@gmail.com>
2025-06-10 10:04:08 +0545

    Filter agent querysets

--- crm/agents/views.py
+++ crm/agents/views.py
@@ -11,7 +11,8 @@ class AgentListView(LoginRequiredMixin, generic.ListView):
     template_name = "agents/agent_list.html"
     
     def get_queryset(self):
-        return Agent.objects.all()
+        organisation = self.request.user.userprofile
+        return Agent.objects.filter(organisation=organisation)
 
 
 class AgentCreateView(LoginRequiredMixin, generic.CreateView):
@@ -20,7 +21,7 @@ class AgentCreateView(LoginRequiredMixin, generic.CreateView):
 
     def get_success_url(self):
         return reverse("agents:agent-list")
-
+    
     def form_valid(self, form):
         agent = form.save(commit=False)
         agent.organisation = self.request.user.userprofile
@@ -33,7 +34,8 @@ class AgentDetailView(LoginRequiredMixin, generic.DetailView):
     context_object_name = "agent"
 
     def get_queryset(self):
-        return Agent.objects.all()
+        organisation = self.request.user.userprofile
+        return Agent.objects.filter(organisation=organisation)
 
 
 class AgentUpdateView(LoginRequiredMixin, generic.UpdateView):
@@ -44,8 +46,8 @@ class AgentUpdateView(LoginRequiredMixin, generic.UpdateView):
         return reverse("agents:agent-list")
 
     def get_queryset(self):
-        return Agent.objects.all()
-
+        organisation = self.request.user.userprofile
+        return Agent.objects.filter(organisation=organisation)
 
 class AgentDeleteView(LoginRequiredMixin, generic.DeleteView):
     template_name = "agents/agent_delete.html"
@@ -55,4 +57,5 @@ class AgentDeleteView(LoginRequiredMixin, generic.DeleteView):
         return reverse("agents:agent-list")
 
     def get_queryset(self):
-        return Agent.objects.all()
+        organisation = self.request.user.userprofile
+        return Agent.objects.filter(organisation=organisation)
