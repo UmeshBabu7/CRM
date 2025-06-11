c6ef7635258efafc6d441c467e76b3310c45f411
Umesh <umeshtamang129@gmail.com>
2025-06-11 15:51:32 +0545

    Password reset

--- crm/leads/urls.py
+++ crm/leads/urls.py
@@ -1,15 +1,21 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import (
-    LandingPageView,
-    LeadListView, LeadDetailView,leadCreateView, LeadUpdateView, LeadDeleteView,SignupView
+from django.contrib.auth.views import (
+    LoginView, 
+    LogoutView, PasswordResetView, 
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
+from .views import(
+    LandingPageView,LeadListView,LeadDetailView,leadCreateView,LeadUpdateView,LeadDeleteView,SignupView
 )
 
 app_name='leads'
 
 urlpatterns = [
-      path('', LandingPageView.as_view(), name='landing-page'),
+    path('', LandingPageView.as_view(), name='landing-page'),
     path('list/', LeadListView.as_view(), name='lead-list'),
     path('<int:pk>/', LeadDetailView.as_view(), name='lead-detail'),
     path('<int:pk>/update/', LeadUpdateView.as_view(), name='lead-update'),
@@ -18,5 +24,10 @@ urlpatterns = [
 
     path('signup/',SignupView.as_view(),name='signup'),
     path('login/',LoginView.as_view(),name='login'),
-    path('logout/',LogoutView.as_view(),name='logout')
+    path('logout/',LogoutView.as_view(),name='logout'),
+
+    path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
+    path('password-reset-done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
