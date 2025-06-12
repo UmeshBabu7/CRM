47b174056c9e27198c6f3e8b4f40823803b637a3
Umesh <umeshtamang129@gmail.com>
2025-06-12 06:38:36 +0545

    Category model

--- crm/leads/models.py
+++ crm/leads/models.py
@@ -22,6 +22,7 @@ class Lead(models.Model):
     age = models.IntegerField(default=0)
     organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     agent = models.ForeignKey("Agent", null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey("Category", null=True, blank=True, on_delete=models.SET_NULL)
 
 
     def __str__(self):
@@ -34,6 +35,13 @@ class Agent(models.Model):
 
     def __str__(self):
         return self.user.email
+    
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)  # New, Contacted, Converted, Unconverted
+
+    def __str__(self):
+        return self.name
     
 
     def post_user_created_signal(sender, instance, created, **kwargs):
